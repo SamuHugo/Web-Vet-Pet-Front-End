11986d650f74cec66d0c4fb4487946667b83d084
Samuel Hugo <90661523+SamuHugo@users.noreply.github.com>
2023-10-10 23:56:59 -0500

    UPDATE V0.3

--- js/prueba.js
+++ js/prueba.js
@@ -1,1 +1,2 @@
-var hola;
+var hola = "GAAAAA";
+alert(hola);
---
+++ js/slider.js
@@ -0,0 +1,62 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+
+function Next(){
+    let sliderSectionFirst =document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%"
+    },500);
+}
+
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+        slider.style.marginLeft = "-100%"
+    },500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 8000);
+
+
+// var interval;
+
+// function starSlide(){
+//     interval = setInterval(Next, 4000);
+// }
+
+// function stopSlide(){
+//     clearInterval(interval);
+// }
+
+// $(function(){
+//     starSlide();
+//     $('#slider').hover(function(){
+//         stopSlide();
+//     },function(){
+//         starSlide();
+//     })
+// });
